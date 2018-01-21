6e4842f3e0c4b44bc14b02ed613bd0286503e1c3
Harry Xu <harryxu@Harrys-MBP.hitronhub.home>
2018-01-20 17:08:16 -0800

    updated

--- Assignment 1.R
+++ Assignment 1.R
@@ -1,4 +1,4 @@
-#setwd("~/Desktop/STAT 443/Webwork and HW/Assignment 1")
+setwd("~/Desktop/STAT 443/Webwork and HW/Assignment 1")
 bradtemp <- read.csv("bradforddata.csv", header = T) 
 tsbrad <- ts(bradtemp$Max.Temp, start = c(1930, 1), frequency = 12)
 plot.ts(tsbrad,
@@ -14,44 +14,42 @@ test <- window(tsbrad, start=c(2015,1), end =c(2017,12))
 
 ##b)
 movingavg <- decompose(training, type = c("additive"))
-#movingavg$figure #this is the estimated seasonal effects
+#movingavg$figure is the estimated seasonal effects
 plot(movingavg)
 
 loessmeth <- stl(training, s.window = "periodic")
-plot(loessmeth)
+plot(loessmeth, main = "Loess Smoothing")
 
 ##c)
 #for moving avg:
 timeframe <- 1:(12*(2014-1950+1)) ##list representing month number from 1 to 780
 matrend <- movingavg$trend
-linmod <- lm(matrend~timeframe)
-linmod
+(summary(linmod <- lm(matrend~timeframe)))
+
 #for loess:
 lodata <- loessmeth$time.series
 lotrend <- lodata[,2]
-lomod <- lm(lotrend~timeframe)
+(summary(lomod <- lm(lotrend~timeframe)))
 loseason <- lodata[,1]
 
-## Predictions
-#for moving avg:
-predtime <- data.frame(781:816) #predicting time month number
+#Predictions for moving avg and loess:
+predtime <- data.frame(781:816) #predicting time month number from Jan 2015 - Dec 2017
 names(predtime) = "timeframe"
 
-linpred <- predict(linmod, newdata = predtime) + movingavg$figure[rep(1:12, 3)] 
+mvpred <- predict(linmod, newdata = predtime) + movingavg$figure[rep(1:12, 3)] 
 lopredict <- predict(lomod, newdata = predtime) + loseason[rep(1:12, 3)]
 
-tslinpred <- ts(linpred, start=c(2015,1), frequency = 12 )
+tsmvpred <- ts(mvpred, start=c(2015,1), frequency = 12 )
 tslopredict <- ts(lopredict, start=c(2015,1), frequency = 12 )
 
 plot.ts(test)
-lines(tslinpred, col = "red")
+lines(tsmvpred, col = "red")
 lines(tslopredict, col = "blue")
 
 
-MSPElin <- mean((linpred - test)^2)
-MSPELo <- mean((lopredict - test)^2)
-print(MSPElin)
-print(MSPELo)
+(MSPEmv <- mean((mvpred - test)^2))
+(MSPELo <- mean((lopredict - test)^2))
+
 ###Findings: similar result?
 
 nontrendpred <- ts(linmod$coefficients[1] + movingavg$figure[rep(1:12, 3)], start=c(2015,1), frequency = 12)
@@ -67,14 +65,18 @@ weather <- read.csv("histWeather.csv", header = T)
 dat <- subset(weather, weather$AirPtCd == "KAVP")
 
 #daily
-zoodailyma <- zoo(dat$Max_TemperatureF, as.Date(dat$Date))
-zoodailymea <- zoo(dat$Mean_TemperatureF, as.Date(dat$Date))
-plot(zoodailyma)
-plot(zoodailymea)
+zoodailymaT <- zoo(dat$Max_TemperatureF, as.Date(dat$Date))
+zoodailymeanW <- zoo(dat$Mean_Wind_SpeedMPH, as.Date(dat$Date))
 
-plot(aggregate(zoodailyma, as.yearmon, mean))
-plot(aggregate(zoodailymea, as.yearmon, mean))
+zoomonmaT <-aggregate(zoodailymaT, as.yearmon, mean)
+zoomonmeanW <- aggregate(zoodailymeanW, as.yearmon, mean)
 
+plot(zoodailymaT, col = "black", main = "Daily Max Temp(F)")
+plot(zoodailymeanW, col = "hot pink", main = "Daily Mean Wind Speed")
+plot(zoomonmaT, col = "black", main = "Monthly Mean Max Temp(F)")
+plot(zoomonmeanW, col = "hot pink", main = "Monthly Mean Mean Wind Speed")
+
+#head(weather)
 #the data shows a periodicity of 1 year, so the series is not staiontary. There does not appear to have a clear trend. 
 #There is no seasonaly variation, 
 
